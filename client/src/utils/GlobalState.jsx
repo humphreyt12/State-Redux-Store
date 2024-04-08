369480acc175acf11470dff05aeadc167ab9ce3b
Tina Humphrey <humphrey14t@gmail.com>
2024-04-07 23:26:07 -0400

    modified changes to GlobalState.jsx, reducers.js, and store.js

--- client/src/utils/GlobalState.jsx
+++ client/src/utils/GlobalState.jsx
@@ -1,23 +1,15 @@
-import { createContext, useContext, useReducer } from "react";
-import { reducer } from './reducers'
+import { Provider } from 'react-redux';
 
-const StoreContext = createContext();
-const { Provider } = StoreContext;
+// Import Redux store from the file we created in /utils
+import store from './store';
 
-const StoreProvider = ({ value = [], ...props }) => {
-  const [state, dispatch] = useReducer(reducer, {
-    products: [],
-    cart: [],
-    cartOpen: false,
-    categories: [],
-    currentCategory: '',
-  });
 
-  return <Provider value={[state, dispatch]} {...props} />;
-};
+export default function StoreProvider(props) {
 
-const useStoreContext = () => {
-  return useContext(StoreContext);
-};
+  // Return the provider component provided by react-redux
+  return <Provider store={store} {...props} />;
+}
 
-export { StoreProvider, useStoreContext };
+
+
+
